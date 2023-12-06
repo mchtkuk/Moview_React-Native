6a6b1c464a494531f9280a256964e23ca52c7cd9
mchtkk <mucahit.kuk@gmail.com>
2023-12-06 23:26:03 +0300

    types and movie api key structured

---
+++ src/config/api.ts
@@ -0,0 +1,35 @@
+import { Axios } from "axios";
+import { GenreIds } from "./movies";
+
+export const apiClient = new Axios({
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    Accept: "Application/json",
+    Authorization:
+      "API KEY HERE",
+  },
+});
+
+export type MovieItem = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: GenreIds[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type DiscoverMoviesPayload = {
+  page: number;
+  results: MovieItem[];
+  total_pages: number;
+  total_results: number;
+};
